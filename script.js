5c6ac0e4f828fb3b77b1745943cf20b413d0904e
Aktaruzzaman <jobneed.aktar@gmail.com>
2025-05-10 10:15:12 +0600

    if else statement

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-var x = 10;
-var y = 10;
-var z = x + y;
-console.log(z);
+// variable
+
+// var x = 10;
+// var y = 10;
+// var z = x + y;
+// console.log(z);
+
+
+// if else statement
+
+let x = 25;
+let city = "Dhaka";
+if(x>18 && city == "Dhaka") {
+    console.log("You are ready to go!");
+}
+
