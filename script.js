fcc66bdb6a0c1b0b568ccbdc5f0b9ceefa5ba6ca
Aktaruzzaman <jobneed.aktar@gmail.com>
2025-05-10 14:25:37 +0600

    for loop and while loop

--- script.js
+++ script.js
@@ -18,41 +18,53 @@
 
 
 // switch case statement
-let marks = parseFloat(prompt("Enter a number : "));
-switch(true) {
-    case(marks>100 || marks<=0):
-    console.log("Invalid marks");
-    break;
+// let marks = parseFloat(prompt("Enter a number : "));
+// switch(true) {
+//     case(marks>100 || marks<=0):
+//     console.log("Invalid marks");
+//     break;
 
-    case(marks>=90 && marks<=100):
-    console.log("A+");
-    break;
+//     case(marks>=90 && marks<=100):
+//     console.log("A+");
+//     break;
 
-    case(marks>=80 && marks<90) :
-    console.log("A");
-    break;
+//     case(marks>=80 && marks<90) :
+//     console.log("A");
+//     break;
 
-    case(marks>=70 && marks<80) :
-    console.log("A-");
-    break;
+//     case(marks>=70 && marks<80) :
+//     console.log("A-");
+//     break;
 
-    case(marks>=60 && marks<70) :
-    console.log("B");
-    break;
+//     case(marks>=60 && marks<70) :
+//     console.log("B");
+//     break;
 
-    case(marks>=50 && marks<60) :
-    console.log("C");
-    break;
+//     case(marks>=50 && marks<60) :
+//     console.log("C");
+//     break;
 
-    case(marks>=40 && marks<50) :
-    console.log("D");
-    break;
+//     case(marks>=40 && marks<50) :
+//     console.log("D");
+//     break;
 
-    case(marks>=33 && marks<40) :
-    console.log("E");
-    break;
+//     case(marks>=33 && marks<40) :
+//     console.log("E");
+//     break;
 
-    default :
-    console.log("Fail");
-}
+//     default :
+//     console.log("Fail");
+// }
 
+
+// for loop
+// for(let x = 0; x<10; x++) {
+//     document.write(x+"<button>for loop</button> </br>");    
+// }
+
+// while loop
+// let x = 0;
+// while(x<10) {
+//     document.write(x+"<button>While loop</button> </br>");
+//     x++;
+// }
