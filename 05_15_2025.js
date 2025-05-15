47bdca9ee55e543063b2702e04f62a3e37e2b2d3
Aktaruzzaman <jobneed.aktar@gmail.com>
2025-05-15 13:49:39 +0600

    for in loop use

---
+++ 05_15_2025.js
@@ -0,0 +1,14 @@
+let info = {
+    "name" : "Google corporation",
+    "location" : "california",
+    "woner" : "larry page & sargray grean",
+    "CEO" : "Sundar pichai"
+}
+
+for(let val in info){
+    // property access
+    // console.log(val);
+
+    // value access
+    console.log(info[val]);
+}
