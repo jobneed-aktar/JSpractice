a56b6b149eeb87d213a94c60145228f3d39a4e4b
Aktaruzzaman <jobneed.aktar@gmail.com>
2025-05-16 17:32:34 +0600

    some of problem

--- 05-16-2025/script.js
+++ 05-16-2025/script.js
@@ -1,12 +1,28 @@
-const person = {
-    name: "john",
-    age: 30,
-    city: "New york",
-    greet: function() {return 30}
-}
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World"
 
-person.greet = person.greet.toString();
-const farid = JSON.stringify(person);
+// function createHelloWorld() {
+//     return function(...args) {
+//         return "Hello World";
+//     }
+// }
 
-document.getElementById("demo").innerHTML = farid;
+// let result = createHelloWorld();
+// console.log(result("what the hell are you doing!"));
 
+
+
+
+
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+
+// function createCounter(n) {
+//     return function() {
+//         return n++;
+//     }
+// }
+
+// let result = createCounter(0);
+// console.log(result());
+// console.log(result());
+// console.log(result());
