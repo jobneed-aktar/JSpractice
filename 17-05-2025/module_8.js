fb2767e6c5740b605a95bee60d853825855e72f0
Aktaruzzaman <jobneed.aktar@gmail.com>
2025-05-17 11:54:38 +0600

    wondow and navigator object

---
+++ 17-05-2025/module_8.js
@@ -0,0 +1,49 @@
+// window object
+// alert
+// function objAlert() {
+//     alert("i'm alert function");
+// }
+
+// confirm
+// function objConfirm() {
+//     const result = confirm("Do you want to delete?");
+//     document.write(result);
+// }
+
+// prompt
+// function objPrompt() {
+//     let result =  prompt("write you name : ");
+//     document.write(result);
+// }
+
+// open
+// function objOpen() {
+//     open();
+// }
+
+// close
+// function objClose() {
+//     close();
+// }
+
+// set time out
+// setTimeout(()=>{
+//     alert("i'm from setTimeout");
+// },1000)
+
+// Javascript navigator :-
+// const appCodeName = navigator.appCodeName;
+// const appName = navigator.appName;
+// const appVersion = navigator.appVersion;
+// const cookieEnabled = navigator.cookieEnabled;
+// const language = navigator.language;
+// const userAgent = navigator.userAgent;
+// const platform = navigator.platform;
+
+// document.writeln(`${appCodeName}</br>`);
+// document.writeln(`${appName}</br>`);
+// document.writeln(`${appVersion}</br>`);
+// document.writeln(`${cookieEnabled}</br>`);
+// document.writeln(`${language}</br>`);
+// document.writeln(`${userAgent}</br>`);
+// document.writeln(`${platform}</br>`);
