99917af2942f8e71f1a1c7b4dfd9eac2f52457ab
Aktaruzzaman <jobneed.aktar@gmail.com>
2025-05-21 07:42:16 +0600

    Anisul islam 2nd  vide has been done about DOM

--- 21-05-25/DOM/script.js
+++ 21-05-25/DOM/script.js
@@ -1,11 +1,24 @@
-// getElementById
-const headingElement = document.getElementById("headingElement");
-const para = document.getElementById("para");
-const heading2 = document.getElementById("heading2");
-const h1Tag = document.getElementsByTagName("h1")[1];
-const paraLast = document.getElementsByClassName("paraLast")[0];
-headingElement.innerText = "Hello";
-para.innerText = "What the hell";
-heading2.innerText = "This is just nothing";
-h1Tag.innerText = "where are you now?";
-paraLast.innerText = "one day i am gonna fly away";
+// getElementById || First video No:  DOM 49
+// const headingElement = document.getElementById("headingElement");
+// const para = document.getElementById("para");
+// const heading2 = document.getElementById("heading2");
+// const h1Tag = document.getElementsByTagName("h1")[1];
+// const paraLast = document.getElementsByClassName("paraLast")[0];
+// headingElement.innerText = "Hello";
+// para.innerText = "What the hell";
+// heading2.innerText = "This is just nothing";
+// h1Tag.innerText = "where are you now?";
+// paraLast.innerText = "one day i am gonna fly away";
+
+
+// const pElement = document.querySelector(".para");
+// const pElement = document.querySelector("#para");
+// const pElement = document.querySelector("p");
+// pElement.innerHTML = "Hello world!";
+
+// const ancor = document.querySelector("a");
+// ancor.textContent = "About";
+// const liElement1 = document.querySelectorAll("ul li")[0].textContent = "India";
+// const liElement2 = document.querySelectorAll("ul li")[1].textContent = "Pakistan";
+
+// const myDiv = document.querySelectorAll(".myDiv a")[0].textContent = "Blogs";
