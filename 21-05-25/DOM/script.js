81840cfa47ba8267447a4ac0f70dab6dfec790a6
Aktaruzzaman <jobneed.aktar@gmail.com>
2025-05-21 07:26:10 +0600

    Anisul islam 1st vide has been done about DOM

---
+++ 21-05-25/DOM/script.js
@@ -0,0 +1,11 @@
+// getElementById
+const headingElement = document.getElementById("headingElement");
+const para = document.getElementById("para");
+const heading2 = document.getElementById("heading2");
+const h1Tag = document.getElementsByTagName("h1")[1];
+const paraLast = document.getElementsByClassName("paraLast")[0];
+headingElement.innerText = "Hello";
+para.innerText = "What the hell";
+heading2.innerText = "This is just nothing";
+h1Tag.innerText = "where are you now?";
+paraLast.innerText = "one day i am gonna fly away";
